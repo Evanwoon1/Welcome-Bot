4b3af91b4d850b5f877837e44acb5ed36dbb79d7
Evanwoon1 <157019948+Evanwoon1@users.noreply.github.com>
2025-03-18 20:25:35 +0800

    config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
     mongoURI : "",
     statusType: 4, // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom
     statusContent: "C", // A = Show total users count, B = Show total servers count, C = Custom message
-    customStatus: "GlaceYT",
+    customStatus: "Watching over new members...",
     streamingURL: "https://www.twitch.tv/glaceytt"
 };
